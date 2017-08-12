8692d8f979636891d9871268972349d77c56d91e
wucheng <wucheng@szba-mobile.com>
2017-08-12 11:16:05 +0800

    图片加载器v1.1

--- ImageLoader/ImageLoader.java
+++ ImageLoader/ImageLoader.java
@@ -16,27 +16,10 @@ import java.util.concurrent.Executors;
 
 public class ImageLoader {
     //图片缓存
-    LruCache<String,Bitmap> mImageCache;
+    ImageCache mImageCache = new ImageCache();
     //线程池，线程数量为CPU数量
     ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-    public ImageLoader () {
-        initImageCache();
-    }
-
-    private void initImageCache() {
-        //计算可使用的最大内存
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory()/1024);
-        //取四分之一的可用内存作为缓存
-        final int cacheSize = maxMemory/4;
-
-        mImageCache = new LruCache<String, Bitmap>(cacheSize){
-            @Override
-            protected int sizeOf(String key, Bitmap value) {
-                return value.getRowBytes() * value.getHeight() / 1024;
-            }
-        };
-    }
 
     public void displayImage(final String url, final ImageView imageView) {
         imageView.setTag(url);
